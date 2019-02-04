e4c9fbed7c58af10e2b3fc0ce7e53f9664ef04f3
Wael Hasan <wael850224@gmail.com>
2019-02-04 22:41:04 +0100

    New

--- src/LudoWebApp/Controllers/HomeController.cs
+++ src/LudoWebApp/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace LudoWebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Home Controllers";
 
             return View();
         }
