cb923664b77d320b1add71054e64de483c0bfb71
Wael Hasan <wael850224@gmail.com>
2019-03-25 10:04:24 +0100

    Input Validation

--- src/WebAppMVC/Models/PlayerModel.cs
+++ src/WebAppMVC/Models/PlayerModel.cs
@@ -10,6 +10,7 @@ namespace WebAppMVC.Models
     public class PlayerModel
     {
         public int PlayerId { get; set; }
+        [Required]
         [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
         public PlayerColor PlayerColor;
